e2db8a278314a45653c3dcc755a76256e9293710
Geremy Condra <CTO@OpenMigration.net>
2009-04-26 21:42:25 -0400

    DOCUMENTATION: updated fsm.py to (hopefully) make it clearer

--- examples/fsm.py
+++ examples/fsm.py
@@ -3,25 +3,24 @@
 from graph.base import Graph
 import sys
 
-def build_fsm():
+def walk_fsm(data):
 	fsm = Graph()
 	fsm.add_edge("consume_one", "loop_on_zero", consumes="1", accept=False)
 	fsm.add_edge("loop_on_zero", "loop_on_zero", consumes="0", accept=False)
 	fsm.add_edge("loop_on_zero", "end_with_one", consumes="1", accept=True)
-	fsm["loop_on_zero"].accept = True
-	fsm.start = "consume_one"
-	return fsm
-
-def walk_fsm(data, fsm):
-	w = fsm.walk_path(fsm.start)
+	accept = False
+	w = fsm.walk_path("consume_one")
 	for edges in w:
-		next = {e.consumes: e for e in edges}.get([data.pop(0)], False)
-		if next: accept = getattr(next.end, "accept", False)
-		else: return accept
-		w.send(next)
+		try:
+			next = {e.consumes: e for e in edges}[data.pop(0)]
+			accept = next.accept
+			w.send(next)
+		except:
+			return False
+	return accept and not data
+
 
 if __name__ == "__main__":
-	fsm = build_fsm()
-	accept = walk_fsm(list(sys.argv[1]), fsm)
+	accept = walk_fsm(list(sys.argv[1]))
 	if accept: print("Accepted!")
 	else: print("Not accepted!")
